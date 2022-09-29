24b22ce5acb24d33dc0c9ee98cfd177a923191af
liguanlin <li.guanlin@mail.nwpu.edu.cn>
2022-09-29 20:58:03 +0800

    update dataset class

--- ldm/data/harmonization_dataset.py
+++ ldm/data/harmonization_dataset.py
@@ -46,6 +46,7 @@ def make_dataset_with_file(root_dir, sub_dir, txt_file):
 
 import torchvision.transforms.functional as tf
 
+"""
 class HarmonizationBase(data.Dataset):
     def __init__(self, data_root, sub_dir, text_file, size=256):
         #imgs = make_dataset(data_root)
@@ -107,7 +108,69 @@ class HarmonizationTrain(HarmonizationBase):
 class HarmonizationValidation(HarmonizationBase):
     def __init__(self, **kwargs):
         super().__init__(data_root="/mnt/lustre/zhaobin/liguanlin/datasets/HAdobe5k/", sub_dir='composite_images', text_file='HAdobe5k_test.txt', **kwargs)
+"""
 
+class HarmonizationBase(data.Dataset):
+    def __init__(self, data_root, sub_dir, size=256):
+        imgs = make_dataset(data_root)
+        #imgs = make_dataset_with_file(data_root, sub_dir, text_file)
+        self.imgs = imgs
+        self.tfs = transforms.Compose([
+                transforms.Resize((size, size)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5, 0.5])
+        ])
+        self.loader = pil_loader
+        self.image_size = size
+        
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5, 0.5])
+        ])
+        self.sub_dir = sub_dir
+
+    def __getitem__(self, index):
+        ret = {}
+        path = self.imgs[index]
+        name_parts=path.split('_')
+        mask_path = self.imgs[index].replace(self.sub_dir,'masks') # #修改点5，如果是带噪声的训练，将这里修改为composite_noisy25_images
+        mask_path = mask_path.replace(('_'+name_parts[-1]),'.png')
+        target_path = self.imgs[index].replace(self.sub_dir,'real_images') # #修改点6，如果是带噪声的训练，将这里修改为composite_noisy25_images
+        target_path = target_path.replace(('_'+name_parts[-2]+'_'+name_parts[-1]),'.jpg')
+
+        comp = Image.open(path).convert('RGB')
+        real = Image.open(target_path).convert('RGB')
+        mask = Image.open(mask_path).convert('1')
+
+        comp = tf.resize(comp, [self.image_size, self.image_size])
+        mask = tf.resize(mask, [self.image_size, self.image_size])
+        #mask = tf.resize(mask, [224, 224]) #对MAE训练，需要将这里修改为224,224
+        real = tf.resize(real,[self.image_size, self.image_size])
+
+        #apply the same transform to composite and real images
+        comp = self.transform(comp)
+        #mask = self.mask_transform(mask)
+        mask = tf.to_tensor(mask)
+
+        real = self.transform(real)
+
+        ret['gt_image'] = real
+        ret['cond_image'] = comp
+        ret['mask'] = mask
+        ret['path'] = path.rsplit("/")[-1]
+
+        return ret
+
+    def __len__(self):
+        return len(self.imgs)
+
+class HarmonizationTrain(HarmonizationBase):
+    def __init__(self, **kwargs):
+        super().__init__(data_root="/mnt/lustre/zhaobin/liguanlin/datasets/iHarmony/HAdobe5k_256/composite_images_train/", sub_dir='composite_images_train', **kwargs)
+
+class HarmonizationValidation(HarmonizationBase):
+    def __init__(self, **kwargs):
+        super().__init__(data_root="/mnt/lustre/zhaobin/liguanlin/datasets/iHarmony/HAdobe5k_256/composite_images_test/", sub_dir='composite_images_test', **kwargs)
 
 class SSHarmonizationTestDataset(data.Dataset):
     def __init__(self, data_root, mask_config={}, data_len=-1, image_size=[256, 256], loader=pil_loader):
